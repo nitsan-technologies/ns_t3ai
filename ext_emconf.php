de7bae2574dd9ce22d52ed61531ed29a459a37b7
NITSAN <info@nitsan.in>
2024-08-20 20:32:26 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF['ns_t3ai'] = [
     'description' => 'Meet T3AI- Experience AI-powered content generation, advanced SEO optimization, creative media production, easy language translation, and more—all within a few clicks, directly from your TYPO3 backend. Maximize your productivity by saving up to four times your time with T3AI.
     
     Explore the Live Demo: https://t3-extension-live.t3planet.com/typo3/?TYPO3_AUTOLOGIN_USER=editor-ns-t3ai
-    Product Page, Documentation & Support: https://t3planet.com/t3ai-typo3-chatgpt-extension',
+    Product Page, Documentation & Support: https://t3planet.com/t3ai-typo3-extension',
     'category' => 'be',
     'author' => 'T3: Nilesh Malankiya, T3: Parth Parmar, T3: Jay Bhalgamiya, T3: Rohan Parmar, QA: Vishal Solanki, Krishna Dhapa',
     'author_email' => 'sanjay@nitsantech.com',
