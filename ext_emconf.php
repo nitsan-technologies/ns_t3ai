bc0249b5de541735fff79c8af5083ee7cb4e81d8
Himanshu Ramavat <himanshu.nitsan@gmail.com>
2024-08-21 17:16:09 +0530

    [TASK] Update logo and description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['ns_t3ai'] = [
     'title' => 'T3AI: All-in-One TYPO3 AI Extension',
-    'description' => 'Meet T3AI- Experience AI-powered content generation, advanced SEO optimization, creative media production, easy language translation, and more—all within a few clicks, directly from your TYPO3 backend. Maximize your productivity by saving up to four times your time with T3AI.
+    'description' => 'Meet T3AI- Experience AI-powered Models with ChatGPT, Gemini, Cloude & Azure AI for content generation, advanced SEO optimization, creative media production, easy language translation, and more—all within a few clicks, directly from your TYPO3 backend. Maximize your productivity by saving up to four times your time with T3AI.
     
     Explore the Live Demo: https://t3-extension-live.t3planet.com/typo3/?TYPO3_AUTOLOGIN_USER=editor-ns-t3ai
     Product Page, Documentation & Support: https://t3planet.com/t3ai-typo3-extension',
@@ -12,7 +12,7 @@ $EM_CONF['ns_t3ai'] = [
     'author_company' => 'T3Planet // NITSAN',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.0.0-13.9.99',
